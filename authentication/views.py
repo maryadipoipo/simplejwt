57fd09cdce08f21b5b4a451b2ea3d8dedcf49909
maryadikpi <maryadi@kpiinstitute.org>
2021-03-30 11:18:51 +0700

    put a login jwt process

--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
+
+
 class RegisterView(GenericAPIView):
     serializer_class = UserSerializer
 
@@ -14,3 +16,12 @@ class RegisterView(GenericAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(GenericAPIView):
+
+    def post(self, request):
+        data = request.data
+        username = data.get('username', '')
+        password = data.get('password', '')
+        pass
